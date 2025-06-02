eae086341b866452ea82cd2fa0ea2a6794062079
beguagua <lomeu10@gmail.com>
2025-06-02 10:33:46 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var emulator = new V86Starter({
+    wasm_path: "bios/v86.wasm",
+    memory_size: 128 * 1024 * 1024,
+    vga_memory_size: 2 * 1024 * 1024,
+    screen_container: document.getElementById("v86-container"),
+    bios: { url: "bios/seabios.bin" },
+    vga_bios: { url: "bios/vgabios.bin" },
+    cdrom: { url: "disks/mikeos.iso" },
+    autostart: true,
+});
